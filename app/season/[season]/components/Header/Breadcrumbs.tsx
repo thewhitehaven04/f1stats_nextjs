766bb7051c431294849efc9ad09b9adc0a6341ae
German <thewhitehaven04@gmail.com>
2025-04-16 01:23:28 +0200

    Update breadcrumb segments filtering

--- app/season/[season]/components/Header/Breadcrumbs.tsx
+++ app/season/[season]/components/Header/Breadcrumbs.tsx
@@ -1,14 +1,17 @@
 "use client"
 import { useSelectedLayoutSegments } from "next/navigation"
 
+const STATIC_SEGMENTS = ["season", "event", "session"]
+
 export const Breadcrumbs = () => {
     const segments = useSelectedLayoutSegments()
+    const breadcrumbSegments = segments.filter(segment => !STATIC_SEGMENTS.includes(segment))
     return (
         <nav className="breadcrumbs">
             <ul>
-                {segments.map((segment) => (
+                {breadcrumbSegments.map(segment => (
                     <li key={segment}>
-                        <a href={segment}>{segment}</a>
+                        <a href={segment}>{decodeURIComponent(segment)}</a>
                     </li>
                 ))}
             </ul>
