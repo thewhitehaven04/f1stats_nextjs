d73fb6db7045665a17ee5b9c244a5b05cb34b052
German <thewhitehaven04@gmail.com>
2025-04-30 00:30:36 +0200

    Update popup card

--- src/components/PopupCard/index.tsx
+++ src/components/PopupCard/index.tsx
@@ -1,4 +1,14 @@
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 import clsx from "clsx"
+import { LucideX } from "lucide-react"
 
 export const PopupCard = (
     props: {
@@ -10,37 +20,19 @@ export const PopupCard = (
 ) => {
     const { className, children, actions, title, onClose, ...rest } = props
     return (
-        <div
-            className={clsx(
-                "card card-sm bg-base-100 border-2 border-neutral-100 border-solid shadow-xl absolute left-0 top-10 min-w-40 py-4",
-                className,
-            )}
-            {...rest}
-        >
-            <div className="flex flex-row justify-between items-start px-4">
-                <div className="card-title">{title}</div>
-                <button type="button" onClick={onClose}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="inline-block h-4 w-4 stroke-current"
-                    >
-                        <title>Close</title>
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M6 18L18 6M6 6l12 12"
-                        />
-                    </svg>
-                </button>
+        <Card className={clsx("absolute left-0 top-10 min-w-40 py-4", className)} {...rest}>
+            <div className="flex flex-row justify-between items-center px-2">
+                <CardHeader>
+                    <CardTitle>{title}</CardTitle>
+                </CardHeader>
+                <Button type="button" size="sm" onClick={onClose}>
+                    <LucideX />
+                </Button>
             </div>
-            {title && <div className="divider w-full px-2 my-0" />}
-            <div className="card-body">
-                {children}
-                <div className="card-actions">{actions}</div>
-            </div>
-        </div>
+            <CardContent>{children}</CardContent>
+            <CardFooter className="flex flex-row justify-end gap-2">
+                <CardAction>{actions}</CardAction>
+            </CardFooter>
+        </Card>
     )
 }
