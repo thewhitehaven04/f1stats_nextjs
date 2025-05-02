653bd096fab261b135ab00e2fced9916fbdbe3ef
German <thewhitehaven04@gmail.com>
2025-05-02 04:22:28 +0200

    Restyle column visibility changer

--- src/components/Table/Toolbars/ColumnVisibilityButton.tsx
+++ src/components/Table/Toolbars/ColumnVisibilityButton.tsx
@@ -1,56 +1,42 @@
 "use client"
-import { PopupCard } from "@/components/PopupCard"
 import { useTableContext } from "@/components/Table/context"
 import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import type { RowData } from "@tanstack/react-table"
-import { useState } from "react"
 
 export function ColumnVisibilityButton() {
     const { getAllLeafColumns } = useTableContext<RowData>()
 
-    const [isColumnCardVisible, setIsColumnCardVisible] = useState(false)
-
     return (
-        <div className="relative z-10">
-            <Button
-                type="button"
-                onClick={() => setIsColumnCardVisible(!isColumnCardVisible)}
-                variant="secondary"
-                size="md"
-            >
-                Columns
-            </Button>
-            {isColumnCardVisible && (
-                <PopupCard
-                    title="Columns"
-                    className="w-max -translate-x-1/2"
-                    onClose={() => setIsColumnCardVisible(false)}
-                >
-                    <div className="grid grid-cols-[repeat(auto-fit,_minmax(96px,_1fr))] grid-rows-8 grid-flow-col justify-stretch gap-x-4 gap-y-2">
-                        {getAllLeafColumns().map(
-                            (column) =>
-                                column.getCanHide() &&
-                                typeof column.columnDef.header === "string" && (
-                                    <label
-                                        key={column.id}
-                                        className="flex flex-row items-center gap-2"
-                                    >
-                                        <input
-                                            type="checkbox"
-                                            checked={column.getIsVisible()}
-                                            onChange={column.getToggleVisibilityHandler()}
-                                            className="checkbox-xs"
-                                        />
-                                        {column.parent &&
-                                            typeof column.parent.columnDef.header === "string" &&
-                                            column.parent.columnDef.header}
-                                        , {column.columnDef.header}
-                                    </label>
-                                ),
-                        )}
-                    </div>
-                </PopupCard>
-            )}
-        </div>
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button type="button" variant="secondary" size="md">
+                    Columns
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="min-w-sm w-full">
+                <h2 className="text-card-foreground mb-6">Columns</h2>
+                <div className="grid grid-cols-[repeat(auto-fit,_minmax(124px,_1fr))] pl-4 grid-rows-8 grid-flow-col justify-stretch gap-6">
+                    {getAllLeafColumns().map(
+                        (column) =>
+                            column.getCanHide() &&
+                            typeof column.columnDef.header === "string" && (
+                                <Label key={column.id} className="font-normal">
+                                    <Checkbox
+                                        checked={column.getIsVisible()}
+                                        onCheckedChange={() => column.toggleVisibility()}
+                                    />
+                                    {column.parent &&
+                                        typeof column.parent.columnDef.header === "string" &&
+                                        column.parent.columnDef.header}
+                                    , {column.columnDef.header}
+                                </Label>
+                            ),
+                    )}
+                </div>
+            </PopoverContent>
+        </Popover>
     )
 }
