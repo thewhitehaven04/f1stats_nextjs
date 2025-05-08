daf3cef9adb653f69aa32688e43a70107dfe46c3
German <thewhitehaven04@gmail.com>
2025-05-09 00:15:56 +0200

    Selection fix

--- app/season/[season]/event/[event]/session/[session]/laps/components/Tabs/Analysis/LapsTableSection/hooks/useLapSelection.ts
+++ app/season/[season]/event/[event]/session/[session]/laps/components/Tabs/Analysis/LapsTableSection/hooks/useLapSelection.ts
@@ -2,14 +2,14 @@ import { useCallback, useState } from "react"
 
 export const useLapSelection = () => {
     const [selection, setSelection] = useState<[string, number][]>([])
-    const hasSelected = !!Object.keys(selection).length
+    const hasSelected = !!selection.length
     const updateSelection = useCallback(
         ({ driver, lap, state }: { driver: string; lap: number; state: boolean }) => {
             if (state) {
                 setSelection((prev) => [...prev, [driver, lap]])
             } else {
                 setSelection((prev) =>
-                    prev.filter(([key, value]) => key !== driver && value !== lap),
+                    prev.filter(([key, value]) => (key !== driver || value !== lap)),
                 )
             }
         },
