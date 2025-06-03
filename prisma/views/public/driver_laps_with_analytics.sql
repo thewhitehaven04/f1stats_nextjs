b8d2fc94e826ef1a88a1a9eb0c722f60593fb2b2
German <thewhitehaven04@gmail.com>
2025-06-03 23:51:36 +0200

    Update bundle size

--- prisma/views/public/driver_laps_with_analytics.sql
+++ prisma/views/public/driver_laps_with_analytics.sql
@@ -1,19 +1,19 @@
 SELECT
-  laps.driver_id,
-  laps.event_name,
-  laps.session_type_id,
-  laps.season_year,
-  min(laps.sector_1_time) AS pb_s1,
-  min(laps.sector_2_time) AS pb_s2,
-  min(laps.sector_3_time) AS pb_s3,
-  max(laps.speedtrap_1) AS pb_st1,
-  max(laps.speedtrap_2) AS pb_st2,
-  max(laps.speedtrap_fl) AS pb_stfl,
-  min(laps.laptime) AS pb_laptime
+  driver_id,
+  event_name,
+  session_type_id,
+  season_year,
+  min(sector_1_time) AS pb_s1,
+  min(sector_2_time) AS pb_s2,
+  min(sector_3_time) AS pb_s3,
+  max(speedtrap_1) AS pb_st1,
+  max(speedtrap_2) AS pb_st2,
+  max(speedtrap_fl) AS pb_stfl,
+  min(laptime) AS pb_laptime
 FROM
   laps
 GROUP BY
-  laps.event_name,
-  laps.session_type_id,
-  laps.season_year,
-  laps.driver_id;
+  event_name,
+  session_type_id,
+  season_year,
+  driver_id;
--- prisma/views/public/laps_with_analytics.sql
+++ prisma/views/public/laps_with_analytics.sql
@@ -1,17 +1,17 @@
 SELECT
-  laps.event_name,
-  laps.session_type_id,
-  laps.season_year,
-  min(laps.sector_1_time) AS min_s1,
-  min(laps.sector_2_time) AS min_s2,
-  min(laps.sector_3_time) AS min_s3,
-  max(laps.speedtrap_1) AS best_st1,
-  max(laps.speedtrap_2) AS best_st2,
-  max(laps.speedtrap_fl) AS best_stfl,
-  min(laps.laptime) AS best_laptime
+  event_name,
+  session_type_id,
+  season_year,
+  min(sector_1_time) AS min_s1,
+  min(sector_2_time) AS min_s2,
+  min(sector_3_time) AS min_s3,
+  max(speedtrap_1) AS best_st1,
+  max(speedtrap_2) AS best_st2,
+  max(speedtrap_fl) AS best_stfl,
+  min(laptime) AS best_laptime
 FROM
   laps
 GROUP BY
-  laps.event_name,
-  laps.session_type_id,
-  laps.season_year;
+  event_name,
+  session_type_id,
+  season_year;
