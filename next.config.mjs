ea3341eace7f7f1b4d2b0008fe3f77b11754d2eb
German <thewhitehaven04@gmail.com>
2025-06-04 23:34:28 +0200

    Update rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -3,23 +3,23 @@ const nextConfig = {
     rewrites: async () => {
         return [
             {
-                source: "/api/py/:path*",
+                source: "/api/:path*",
                 destination:
                     process.env.NODE_ENV === "development"
                         ? "http://127.0.0.1:8000/api/py/:path*"
-                        : "/api/py/:path*",
+                        : "/api/",
             },
             {
                 source: "/docs",
                 destination:
-                    process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/docs" : "/docs",
+                    process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/docs" : "/api/docs",
             },
             {
                 source: "/openapi.json",
                 destination:
                     process.env.NODE_ENV === "development"
                         ? "http://127.0.0.1:8000/openapi.json"
-                        : "/openapi.json",
+                        : "/api/openapi.json",
             },
         ]
     },
