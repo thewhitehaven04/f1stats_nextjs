e17a5ba0f4d69dbf491bcd0966f35360e31b3d5b
German <thewhitehaven04@gmail.com>
2025-06-05 00:54:32 +0200

    Update rewrites

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,5 @@
 import { createClient, createConfig } from "@hey-api/client-fetch"
 
 export const ApiClient = createClient(createConfig({
-    baseUrl: "http://localhost:3000",
+    baseUrl: "",
 }))
