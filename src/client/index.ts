3e4399669fe4826b39ff3b6bf7cacf3bb9859c7a
German <thewhitehaven04@gmail.com>
2025-06-10 17:13:11 +0200

    Update public URL

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,7 +2,7 @@ import { createClient } from "@hey-api/client-fetch"
 
 const url =
     process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-        ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+        ? process.env.NEXT_PUBLIC_URL
         : process.env.NEXT_PUBLIC_BASE_URL
           ? `https://${process.env.NEXT_PUBLIC_BASE_URL}/`
           : "http://localhost:3000/"
@@ -11,4 +11,5 @@ export const ApiClient = createClient({
     baseUrl: url,
 })
 
-console.log('base url: ', url)
+console.log('Current env: ', process.env.NEXT_PUBLIC_VERCEL_ENV)
+console.log('Current base url: ', url)
