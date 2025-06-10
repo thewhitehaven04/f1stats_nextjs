d69847d0e5be5a6627297b263e92a06102e1432b
German <thewhitehaven04@gmail.com>
2025-06-10 02:08:28 +0200

    Fix client url for production env

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,9 +1,14 @@
 import { createClient } from "@hey-api/client-fetch"
 
+const url =
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+        ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+        : process.env.NEXT_PUBLIC_BASE_URL
+          ? `https://${process.env.NEXT_PUBLIC_BASE_URL}/`
+          : "http://localhost:3000/"
+
 export const ApiClient = createClient({
-    baseUrl: process.env.NEXT_PUBLIC_BASE_URL
-        ? `https://${process.env.NEXT_PUBLIC_BASE_URL}/`
-        : "http://localhost:3000/",
+    baseUrl: url,
 })
 
-console.log(process.env.NEXT_PUBLIC_BASE_URL)
+console.log('base url: ', url)
