ed776781cd2a6f95ddd2cfddacf99b3f4e6025f4
German <thewhitehaven04@gmail.com>
2025-09-02 01:20:58 +0200

    Chart responsiveness

--- app/season/[season]/event/[event]/session/[session]/laps/components/Tabs/BoxPlot/Chart.tsx
+++ app/season/[season]/event/[event]/session/[session]/laps/components/Tabs/BoxPlot/Chart.tsx
@@ -5,7 +5,7 @@ import { useMemo } from "react"
 import { Chart } from "react-chartjs-2"
 import { initGlobalChartConfig } from "@/components/Chart/config"
 import { getColorFromColorMap } from "@/components/Chart/helpers"
-import type { SessionLapsData } from '@/client/generated'
+import type { SessionLapsData } from "@/client/generated"
 initGlobalChartConfig()
 
 export function LapsBoxChart({
@@ -15,7 +15,7 @@ export function LapsBoxChart({
 }: {
     isOutliersShown: boolean
     selectedStints: Record<string, number | undefined>
-    laps: SessionLapsData 
+    laps: SessionLapsData
 }) {
     const { color_map } = laps
     const sessionData = useMemo(
@@ -68,88 +68,92 @@ export function LapsBoxChart({
         : laps.low_decile || 0
 
     return (
-        <Chart
-            type="boxplot"
-            data={sessionData}
-            height={laps.driver_lap_data.length * 25}
-            options={{
-                responsive: true,
-                scales: {
-                    x: {
-                        bounds: "ticks",
-                        min: selectionMin,
-                        ticks: {
-                            callback(tickValue) {
-                                return typeof tickValue === "number"
-                                    ? formatTime(tickValue)
-                                    : tickValue
+        <div className="relative w-full flex flex-col">
+            <Chart
+                type="boxplot"
+                data={sessionData}
+                height={laps.driver_lap_data.length * 25}
+                options={{
+                    responsive: true,
+                    resizeDelay: 100,
+                    maintainAspectRatio: false,
+                    scales: {
+                        x: {
+                            bounds: "ticks",
+                            min: selectionMin,
+                            ticks: {
+                                callback(tickValue) {
+                                    return typeof tickValue === "number"
+                                        ? formatTime(tickValue)
+                                        : tickValue
+                                },
                             },
                         },
                     },
-                },
-                elements: {
-                    boxandwhiskers: {
-                        borderWidth: 2,
-                        backgroundColor: "rgba(190, 190, 190, 0.12)",
-                        itemRadius: 5,
-                        itemHitRadius: 5,
-                        itemStyle: "circle",
-                        itemBorderWidth: 1.5,
-                        itemBorderColor(ctx) {
-                            if (typeof ctx.dataset.borderColor === "string") {
-                                return ctx.dataset.borderColor
-                            }
-                            return "grey"
-                        },
-                        meanStyle: "rectRot",
-                        meanBorderColor(ctx) {
-                            if (typeof ctx.dataset.borderColor === "string") {
-                                return ctx.dataset.borderColor
-                            }
-                            return "grey"
-                        },
-                        meanBorderWidth: 1.5,
-                        meanRadius: 10,
-                    },
-                },
-                plugins: {
-                    zoom: {
-                        limits: {
-                            x: {
-                                min: laps.min_time || 0 - 1,
-                                max: laps.max_time || 0 + 1,
+                    elements: {
+                        boxandwhiskers: {
+                            borderWidth: 2,
+                            backgroundColor: "rgba(190, 190, 190, 0.12)",
+                            itemRadius: 5,
+                            itemHitRadius: 5,
+                            itemStyle: "circle",
+                            itemBorderWidth: 1.5,
+                            itemBorderColor(ctx) {
+                                if (typeof ctx.dataset.borderColor === "string") {
+                                    return ctx.dataset.borderColor
+                                }
+                                return "grey"
                             },
+                            meanStyle: "rectRot",
+                            meanBorderColor(ctx) {
+                                if (typeof ctx.dataset.borderColor === "string") {
+                                    return ctx.dataset.borderColor
+                                }
+                                return "grey"
+                            },
+                            meanBorderWidth: 1.5,
+                            meanRadius: 10,
                         },
+                    },
+                    plugins: {
                         zoom: {
-                            drag: {
-                                enabled: true,
+                            limits: {
+                                x: {
+                                    min: laps.min_time || 0 - 1,
+                                    max: laps.max_time || 0 + 1,
+                                },
                             },
-                            mode: "x",
+                            zoom: {
+                                drag: {
+                                    enabled: true,
+                                },
+                                mode: "x",
+                            },
+                        },
+                        tooltip: {
+                            callbacks: {
+                                /** @ts-ignore */
+                                label: ({
+                                    dataset,
+                                    dataIndex,
+                                }: {
+                                    dataset: (typeof sessionData)["datasets"][number]
+                                    dataIndex: number
+                                }) =>
+                                    `Min time: ${formatTime(dataset.data[dataIndex].min || 0)}, ` +
+                                    `max time: ${formatTime(dataset.data[dataIndex].max || 0)}, ` +
+                                    `25% quantile: ${formatTime(dataset.data[dataIndex].q1 || 0)}, ` +
+                                    `75% quantile: ${formatTime(dataset.data[dataIndex].q3 || 0)}, ` +
+                                    `mean: ${formatTime(dataset.data[dataIndex].mean || 0)}, ` +
+                                    `median: ${formatTime(dataset.data[dataIndex].median || 0)}`,
+                            },
                         },
                     },
-                    tooltip: {
-                        callbacks: {
-                            /** @ts-ignore */
-                            label: ({
-                                dataset,
-                                dataIndex,
-                            }: {
-                                dataset: (typeof sessionData)["datasets"][number]
-                                dataIndex: number
-                            }) =>
-                                `Min time: ${formatTime(dataset.data[dataIndex].min || 0)}, ` +
-                                `max time: ${formatTime(dataset.data[dataIndex].max || 0)}, ` +
-                                `25% quantile: ${formatTime(dataset.data[dataIndex].q1 || 0)}, ` +
-                                `75% quantile: ${formatTime(dataset.data[dataIndex].q3 || 0)}, ` +
-                                `mean: ${formatTime(dataset.data[dataIndex].mean || 0)}, ` +
-                                `median: ${formatTime(dataset.data[dataIndex].median || 0)}`,
-                        },
-                    },
-                },
-                indexAxis: "y",
-                minStats: "whiskerMin",
-                maxStats: "whiskerMax",
-            }}
-        />
+                    indexAxis: "y",
+                    minStats: "whiskerMin",
+                    maxStats: "whiskerMax",
+                }}
+            />
+        </div>
     )
 }
--- app/season/[season]/event/[event]/session/[session]/laps/components/Tabs/LapsLinePlot/Chart.tsx
+++ app/season/[season]/event/[event]/session/[session]/laps/components/Tabs/LapsLinePlot/Chart.tsx
@@ -19,98 +19,106 @@ export const LineLapsChart = (
         }
     },
 ) => (
-    <ThemedChart
-        {...merge(
-            {
-                type: "line" as const,
-                height: 150,
-                options: {
-                    elements: {
-                        point: {
-                            radius: 7,
-                            hoverRadius: 7,
-                            borderWidth: 2,
-                            hoverBorderWidth: 2,
-                            backgroundColor(ctx) {
-                                const data = ctx.dataset.data[ctx.dataIndex] as TLapsLinePlotDataset
-                                return TYRE_COLOR_MAP[data.compound] || "grey"
+    <div className="relative w-full h-[500px]">
+        <ThemedChart
+            {...merge(
+                {
+                    type: "line" as const,
+                    options: {
+                        resizeDelay: 100,
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        elements: {
+                            point: {
+                                radius: 7,
+                                hoverRadius: 7,
+                                borderWidth: 2,
+                                hoverBorderWidth: 2,
+                                backgroundColor(ctx) {
+                                    const data = ctx.dataset.data[
+                                        ctx.dataIndex
+                                    ] as TLapsLinePlotDataset
+                                    return TYRE_COLOR_MAP[data.compound] || "grey"
+                                },
+                                borderColor(ctx) {
+                                    const { style, color } =
+                                        ctx.dataset as TLinePlotTabBoxChartDataset
+                                    return style === "default" ? color : getAlternativeColor(color)
+                                },
                             },
-                            borderColor(ctx) {
-                                const { style, color } = ctx.dataset as TLinePlotTabBoxChartDataset
-                                return style === "default" ? color : getAlternativeColor(color)
-                            },
-                        },
-                        line: {
-                            borderColor(ctx) {
-                                const { style, color } = ctx.dataset as TLinePlotTabBoxChartDataset
-                                return style === "default" ? color : getAlternativeColor(color)
-                            },
-                        },
-                    },
-                    interaction: {
-                        ...props.options?.interaction,
-                        mode: "index",
-                        intersect: false,
-                    },
-                    scales: {
-                        y: {
-                            type: "linear",
-                            title: {
-                                text: "Lap time (s)",
-                                display: true,
-                            },
-                            bounds: "ticks",
-                            ticks: {
-                                callback(tickValue) {
-                                    return typeof tickValue === "number"
-                                        ? formatTime(tickValue)
-                                        : tickValue
+                            line: {
+                                borderColor(ctx) {
+                                    const { style, color } =
+                                        ctx.dataset as TLinePlotTabBoxChartDataset
+                                    return style === "default" ? color : getAlternativeColor(color)
                                 },
                             },
                         },
-                        x: {
-                            type: "linear",
-                            title: {
-                                display: true,
-                                text: "Lap number",
-                            },
-                            ticks: {
-                                align: "center",
-                                maxTicksLimit: 20,
-                                stepSize: 3,
-                            },
-                            bounds: "ticks",
+                        interaction: {
+                            ...props.options?.interaction,
+                            mode: "index",
+                            intersect: false,
                         },
-                    },
-                    plugins: {
-                        tooltip: {
-                            callbacks: {
-                                label({ dataset, dataIndex }: TooltipItem<"line">) {
-                                    const item = dataset.data[dataIndex] as TLapsLinePlotDataset
-                                    return `${dataset.label}: ${formatTime(item.y)} (${item.compound})`
+                        scales: {
+                            y: {
+                                type: "linear",
+                                title: {
+                                    text: "Lap time (s)",
+                                    display: true,
                                 },
-                                title(tooltipItems: TooltipItem<"line">[]) {
-                                    return `Lap ${tooltipItems[0].dataIndex}`
+                                bounds: "ticks",
+                                ticks: {
+                                    callback(tickValue) {
+                                        return typeof tickValue === "number"
+                                            ? formatTime(tickValue)
+                                            : tickValue
+                                    },
                                 },
                             },
+                            x: {
+                                type: "linear",
+                                title: {
+                                    display: true,
+                                    text: "Lap number",
+                                },
+                                ticks: {
+                                    align: "center",
+                                    maxTicksLimit: 20,
+                                    stepSize: 3,
+                                },
+                                bounds: "ticks",
+                            },
                         },
-                        zoom: {
+                        plugins: {
+                            tooltip: {
+                                callbacks: {
+                                    label({ dataset, dataIndex }: TooltipItem<"line">) {
+                                        const item = dataset.data[dataIndex] as TLapsLinePlotDataset
+                                        return `${dataset.label}: ${formatTime(item.y)} (${item.compound})`
+                                    },
+                                    title(tooltipItems: TooltipItem<"line">[]) {
+                                        return `Lap ${tooltipItems[0].dataIndex}`
+                                    },
+                                },
+                            },
                             zoom: {
-                                mode: "x",
-                                drag: {
-                                    enabled: true,
+                                zoom: {
+                                    mode: "x",
+                                    drag: {
+                                        enabled: true,
+                                    },
                                 },
-                            },
-                            limits: {
-                                x: {
-                                    minRange: 2,
+                                limits: {
+                                    x: {
+                                        minRange: 2,
+                                    },
                                 },
                             },
                         },
                     },
-                },
-            } satisfies Omit<ChartProps<"line">, "data">,
-            props,
-        )}
-    />
+                } satisfies Omit<ChartProps<"line">, "data">,
+                props,
+            )}
+        />
+    </div>
 )
